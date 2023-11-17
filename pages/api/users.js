57306778e2a55b2a8d954d67aa4f31589c8b3bd9
Bennett Kautz <92739107+bkautz0824@users.noreply.github.com>
2023-11-16 19:48:50 -0500

    removed models

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,15 +3,16 @@
 import {clientPromise} from "../../lib/mongodb";
 import mongoose from "mongoose";
 // Create a global UserModel variable
-import { UserModel } from "../../lib/mongodb";
+
 
 export default async function handler(req, res) {
-  // Reuse the client and UserModel variables
-  const client = await clientPromise;
-  const db = client.db("GYM-GUARDIAN");
-  const User = UserModel;
-  console.log("here above")
-  // Switch on the HTTP method
+   // Reuse the client and UserModel variables
+   console.log("10");
+   const client = await clientPromise;
+   const db = client.db("GYM-GUARDIAN");
+   console.log("11")
+   // Use the User model to interact with the database
+   // Switch on the HTTP method
   switch (req.method) {
     case "POST":
       console.log("here")
@@ -22,26 +23,29 @@ export default async function handler(req, res) {
         _id: new mongoose.Types.ObjectId(),
       };
 
+    
+      let data = await db.collection("User").insertOne(userFields);
       
-      const user = new User(userFields);
-      let data = await db.collection("User").insertOne(user);
-      
-      console.log(user)
+      console.log(userFields)
       console.log(data)
-      const allUsers = await db.collection("User").find({}).toArray();
+      // const allUsers = await db.collection("User").find({}).toArray();
       // Save the user to the database
       // await user.save();
 
       // Respond with the created user
-      res.json(user);
+      res.json(userFields);
       break;
 
     case "GET":
-      // Find all users in the database
-      const users = await User.find({})
-
-      // Respond with the users
-      res.json({ status: 200, data: users });
+      try {
+        const usersCursor = await db.collection("User").find({});
+        const users = await usersCursor.toArray();
+        console.log(users);
+        res.json({ status: 200, data: users });
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
       break;
   }
 }
